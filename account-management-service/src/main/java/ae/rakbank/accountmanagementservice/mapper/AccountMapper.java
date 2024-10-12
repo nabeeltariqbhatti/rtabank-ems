e8d3c1eddcfdee66bc7fc30106dc7acfa40b1266
nabeel.bhatti <tariq.nabeel@systemsltd.com>
2024-10-12 14:36:48 +0500

    accot management added

---
+++ account-management-service/src/main/java/ae/rakbank/accountmanagementservice/mapper/AccountMapper.java
@@ -0,0 +1,39 @@
+package ae.rakbank.accountmanagementservice.mapper;
+
+import ae.rakbank.accountmanagementservice.dto.AccountDTO;
+import ae.rakbank.accountmanagementservice.model.Account;
+
+public class AccountMapper {
+
+
+    public static AccountDTO toAccountDto(Account account) {
+        if (account == null) {
+            return null;
+        }
+        return AccountDTO.builder()
+                .id(account.getId())
+                .firstName(account.getFirstName())
+                .lastName(account.getLastName())
+                .email(account.getEmail())
+                .phone(account.getPhone())
+                .birthDate(account.getBirthDate())
+                .registeredAt(account.getRegisteredAt())
+                .build();
+    }
+
+
+    public static Account toAccountEntity(AccountDTO accountDTO) {
+        if (accountDTO == null) {
+            return null;
+        }
+        Account account = new Account();
+        account.setId(accountDTO.getId());
+        account.setFirstName(accountDTO.getFirstName());
+        account.setLastName(accountDTO.getLastName());
+        account.setEmail(accountDTO.getEmail());
+        account.setPhone(accountDTO.getPhone());
+        account.setBirthDate(accountDTO.getBirthDate());
+        account.setRegisteredAt(accountDTO.getRegisteredAt());
+        return account;
+    }
+}
