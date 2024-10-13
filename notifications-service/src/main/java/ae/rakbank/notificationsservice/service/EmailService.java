7d2bc55edada52cada8697ead7ff43a6d7c75554
nabeel.bhatti <tariq.nabeel@systemsltd.com>
2024-10-14 00:41:56 +0500

    notification service added and crud added

--- notifications-service/src/main/java/ae/rakbank/notificationsservice/service/EmailService.java
+++ notifications-service/src/main/java/ae/rakbank/notificationsservice/service/EmailService.java
@@ -1,16 +1,20 @@
 package ae.rakbank.notificationsservice.service;
 
 import ae.rakbank.notificationsservice.dto.EmailRequest;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
 
 @Service
 @Slf4j
@@ -21,17 +25,23 @@ public class EmailService {
 
     @Value("${api.key}")
     private String apiKey;
-    private final RestTemplate restTemplate = new RestTemplate();
 
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void sendEmail(EmailRequest emailRequest) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("accept", "application/json");
-        headers.set("api-key", "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");
-        headers.set("content-type", "application/json");
-        HttpEntity<EmailRequest> requestEntity = new HttpEntity<>(emailRequest, headers);
-        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, String.class);
-       log.info("Response: " + response.getBody());
+        MimeMessagePreparator mimeMessagePreparator =mimeMessage -> {
+            MimeMessageHelper mimeMessageHelper= new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setFrom("info@ipurvey.com");
+            mimeMessageHelper.setSubject(emailRequest.getSubject());
+            mimeMessageHelper.addTo(emailRequest.getTo().get(0).getEmail());
+            mimeMessageHelper.setText(emailRequest.getHtmlContent(),true);
+        };
+        mailSender.send(mimeMessagePreparator);
+       log.info("Response: email sent " );
 
     }
 }
